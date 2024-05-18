093623a4b8a9c94f1f1792e32b0f7245488f8579
SnlperStripes <fabiogr@gmx.de>
2024-05-18 17:40:09 +0200

    feat: add new window startup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Create an event loop
+    let event_loop = EventLoop::new();
+
+    // Create a window
+    let window = WindowBuilder::new()
+        .with_title("Hello, Window!")
+        .with_inner_size(winit::dpi::LogicalSize::new(800.0, 600.0))
+        .build(&event_loop)
+        .unwrap();
+
+    // Run the event loop
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            _ => (),
+        }
+    });
 }
