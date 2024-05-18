8854811c2b473919038f5371e5c5bdbb5b55b95b
SnlperStripes <fabiogr@gmx.de>
2024-05-18 18:45:01 +0200

    feat: add "wasd" movement

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use ggez::{Context, ContextBuilder, GameResult};
-use ggez::event::{self, EventHandler};
+use ggez::event::{self, EventHandler, KeyCode, KeyMods};
 use ggez::graphics::{self, Color, DrawMode, Rect};
 
-struct MainState;
+struct MainState {
+    pos_x: f32,
+    pos_y: f32,
+}
 
 impl MainState {
     fn new() -> GameResult<MainState> {
-        let s = MainState;
+        let s = MainState {
+            pos_x: 350.0,
+            pos_y: 250.0,
+        };
         Ok(s)
     }
 }
@@ -16,12 +22,22 @@ impl EventHandler for MainState {
         Ok(())
     }
 
+    fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods, _repeat: bool) {
+        match keycode {
+            KeyCode::W => self.pos_y -= 5.0,
+            KeyCode::A => self.pos_x -= 5.0,
+            KeyCode::S => self.pos_y += 5.0,
+            KeyCode::D => self.pos_x += 5.0,
+            _ => (),
+        }
+    }
+
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         // Set the background color
         graphics::clear(ctx, Color::from_rgb(0, 0, 0));
 
         // Create a rectangle (square)
-        let square = Rect::new(350.0, 250.0, 100.0, 100.0);
+        let square = Rect::new(self.pos_x, self.pos_y, 100.0, 100.0);
 
         // Set the color and draw the square
         let color = Color::from_rgb(255, 0, 0);
@@ -37,7 +53,7 @@ impl EventHandler for MainState {
 
 fn main() -> GameResult {
     // Create a context and event loop
-    let (mut ctx, event_loop) = ContextBuilder::new("hello_ggez", "Author")
+    let (mut ctx, event_loop) = ContextBuilder::new("Squaro", "Author")
         .window_setup(ggez::conf::WindowSetup::default().title("Hello, ggez!"))
         .window_mode(ggez::conf::WindowMode::default().dimensions(800.0, 600.0))
         .build()?;
